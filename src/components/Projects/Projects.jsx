a60bd66a9e0d7248b22ff11d7991a8d46955de16
Rata Fabian <rata.fabian1@gmail.com>
2023-07-19 14:41:36 +0100

    Your commit message here

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,57 +1,30 @@
 import "./Projects.css"
-import { firstProject, firstProjectSmall, mistery, misterySmall, } from "../../assets"
+import { useEffect, useState } from "react"
+import {  mistery, misterySmall, } from "../../assets"
 import { LazyLoadImage } from "react-lazy-load-image-component"
 import "react-lazy-load-image-component/src/effects/blur.css"
+import ShowProject from "../ShowProject/ShowProject"
+//data
+import  data  from "../../assets/projData.json"
 
 const Projects = () => {
-  
+const [proj1, setProj1] = useState([])
+const [proj2, setProj2] = useState([])
+const [proj3, setProj3] = useState([])
+
+useEffect(()=> {
+  setProj3(data.thirdProject)
+  setProj2(data.secondProject)
+ setProj1(data.firstProject)
+}, [])
+
   return (
     <div className="projects__main-container" id="first-project">
         <div className="projects__sub_container">
-          <div className="projects__first_projects">
-           <h1>Presenting my latest project</h1>
-
-            <div className="first_project_text">
-            <h2>Key Features</h2>
-
-            <div>
-              <i className="fa-solid fa-gauge-high fa-2xl" style={{color: "#E74C3C"}}></i>
-              <p><span>Context-driven Architecture:</span> Employing React's Context API to efficiently manage and share data across components, eliminating the need for prop drilling.</p>
-              </div>
-
-            <div>
-              <i className="fa-solid fa-ruler-horizontal fa-2xl" style={{color: "#16A085 "}}></i>
-              <p><span>Responsive Design:</span> Delivering a seamless user experience on any device, ensuring optimal usability and visual appeal.</p>
-              </div>
-
-            <div>
-              <i className="fa-regular fa-image fa-2xl" style={{color: "#2980B9"}}></i>
-              <p><span>Efficient Image Loading:</span> Implementing lazy loading techniques to enhance performance and minimize loading times.</p>
-              </div>
-
-            <div>
-              <i className="fa-sharp fa-solid fa-bolt fa-2xl" style={{color: "#F1C40F"}}></i>
-              <p><span>Streamlined Navigation:</span> Utilizing React Router for smooth and intuitive page transitions, ensuring a fluid browsing experience.</p>
-              </div>
-
-            </div>
-            <div className="first_project_img">
-              <a href="https://rafatabian.github.io/shop/">
-              <LazyLoadImage
-                    src={firstProject}
-                    placeholderSrc={firstProjectSmall}
-                    effect="blur"
-                    alt="project" 
-                />
-                </a>
-            </div>
-            <div className="github_icon">
-              <a href="https://github.com/rafatabian/shop">
-              <i className="fa-brands fa-github fa-2xl" style={{color: "#ffffff"}}></i>
-              </a>
-               
-            </div>
-          </div>
+          
+        <ShowProject props={proj3}/>
+        <ShowProject props={proj2}/>
+        <ShowProject props={proj1}/>
 
           <div className="projects__second_projects">
             
@@ -59,8 +32,8 @@ const Projects = () => {
              <h2>Coming Soon...</h2>
              
               <div>
-                <i className="fa-brands fa-spotify fa-2xl" style={{color: " #1ED760"}}></i>
-                <p>Utilizing the powerful features of the Spotify API, my primary objective is to develop a cutting-edge music application that not only harnesses the vast musical library of Spotify but also offers a seamlessly intuitive and captivating user experience</p>
+              <i className="fa-solid fa-shirt fa-2xl" style={{color: "#f1f44e"}}></i>
+                <p>Introducing AR Wardrobe - Your Virtual Fitting Room. Try on clothing and accessories virtually in real-time. A revolutionary way to shop fashion online. Coming Soon. Stay tuned!</p>
               </div>
              </div>
                 <div className="second_project_img">
---
+++ src/components/ShowProject/ShowProject.jsx
@@ -0,0 +1,68 @@
+import "./ShowProject.css"
+import { LazyLoadImage } from "react-lazy-load-image-component"
+import "react-lazy-load-image-component/src/effects/blur.css"
+
+import carsProject from "../../assets/first-project.png"
+import carsProjectSmall from "../../assets/first-project-small.png"
+import musicProject from "../../assets/your_music.png"
+import musicProjectTiny from "../../assets/your_music_tiny.png"
+import assistantProject from "../../assets/your_assistent.png"
+import assistantProjectSmall from "../../assets/your_assistent_tiny.png"
+
+
+
+const ShowProject = ({props}) => {
+
+
+  if(props && props.titles){
+
+  return (
+    <div className="projects__first_projects">
+           <h1>{props.titles[0]}</h1>
+
+            <div className="first_project_text">
+            <h2>{props.titles[1]}</h2>
+
+            <div>
+            <i className={props.icons[0]} style={{color:`${props.icons[1]}`}}></i>
+              <p><span>{props.spans[0]}</span>{props.paragraphs[0]}</p>
+              </div>
+
+            <div>
+            <i className={props.icons[2]} style={{color:`${props.icons[3]}`}}></i>
+              <p><span>{props.spans[1]}</span>{props.paragraphs[1]}</p>
+              </div>
+
+            <div>
+            <i className={props.icons[4]} style={{color:`${props.icons[5]}`}}></i>              
+            <p><span>{props.spans[2]}</span>{props.paragraphs[2]}</p>
+              </div>
+
+            <div>
+            <i className={props.icons[6]} style={{color:`${props.icons[7]}`}}></i>              
+            <p><span>{props.spans[3]}</span>{props.paragraphs[3]}</p>
+              </div>
+
+            </div>
+            <div className="first_project_img">
+              <a href={props.ghLinks[0]}>
+              <LazyLoadImage
+                    src={props.images[0] == "cars" ? carsProject : props.images[0] == "music" ? musicProject : assistantProject}
+                    placeholderSrc={props[0] == "cars" ? carsProjectSmall : props[0] == "music" ? musicProjectTiny : assistantProjectSmall}
+                    effect="blur"
+                    alt="projects" 
+                />
+                </a>
+            </div>
+            <div className="github_icon">
+              <a href={props.ghLinks[1]}>
+              <i className={props.icons[8]} style={{color:`${props.icons[9]}`}}></i>
+              </a>
+               
+            </div>
+          </div>
+  )
+}
+}
+
+export default ShowProject
